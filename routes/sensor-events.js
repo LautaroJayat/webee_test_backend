7cab0bbf3d4814db72a611f0208dd47f78daf907
Lautaro Jayat <lautaro.jayat@gmail.com>
2020-08-15 17:47:26 -0300

    Fixed some imports in sensor events routes Started tests dor sensor events

--- routes/sensor-events.js
+++ routes/sensor-events.js
@@ -6,6 +6,9 @@ const createOne = require("../controllers/sensors-event/create");
 const getAll = require("../controllers/sensors-event/get-all");
 const getById = require("../controllers/sensors-event/get-by-id");
 const getBySensorId = require("../controllers/sensors-event/get-by-sensor-id");
+const updateOne = require('../controllers/sensors-event/update-one')
+const deleteOne = require('../controllers/sensors-event/delete-one')
+
 
 // IMPORTING MIDDLEWARES
 const checkValidParentSensor = require("../middlewares/sensors-event/check-parent-sensor");
@@ -13,8 +16,9 @@ const checkValidParentSensor = require("../middlewares/sensors-event/check-paren
 // SETTING ROUTES
 router.get("/", getAll);
 router.get("/:_id", getById);
-router.post("/by-sensor/:_id", checkValidParentSensor, getBySensorId);
-router.put("/:_id", checkValidParentSensor, createOne);
-router.delete("/:_id", createOne);
+router.post("/:_id", checkValidParentSensor,createOne);
+router.get("/by-sensor/:_id", checkValidParentSensor, getBySensorId);
+router.put("/:_id", checkValidParentSensor, updateOne);
+router.delete("/:_id", deleteOne);
 
 module.exports = router;
---
+++ tests/sensor-events/create-one.test.js
@@ -0,0 +1,85 @@
+// LOADING ENVIROMENT VARIABLES
+if (!process.env.DB) {
+  const loadEnvs = require("../../configs/load-envs");
+  const result = loadEnvs();
+
+  // Exiting if no .env
+  if (!result) {
+    console.error(
+      "Could Not load enviroment variables, please check if .env exists or if npm scripts exports correct ENV_FILE variable"
+    );
+    return process.exit(1);
+  }
+}
+
+// IMPORTING DEPENDENCIES
+const mongoose = require("mongoose");
+const request = require("supertest");
+const initDB = require("../../configs/database");
+const { simpleJWTSign } = require("../../configs/encryption");
+const app = require("../../app");
+const { sensorModel } = require("../../models/sensor");
+const { sensorEventModel } = require("../../models/sensor-event");
+
+// TEST SUITE
+describe("POST /sensors", () => {
+  // Mocha hooks
+  before(async () => {
+    await initDB(process.env.DB);
+    await sensorModel.deleteMany({});
+    await sensorEventModel.deleteMany({});
+  });
+  after(async () => {
+    await sensorModel.deleteMany({});
+    await sensorEventModel.deleteMany({});
+    await mongoose.connection.close((err) => {
+      if (err) {
+        console.error("there was an error while closing DB connection", err);
+      }
+    });
+  });
+
+  // Test cases
+  it("Should return 200 and a sensorEvent when everything is OK", (done) => {
+    const sensor = new sensorModel({
+      name: "Test Sensor to update",
+      location: { latitude: 92.123, longitude: 12.699 },
+      minValue: 90,
+      maxValue: 60,
+    });
+    sensor.save().then((s) => {
+      simpleJWTSign({ payload: "mocking payload" }).then((JWT) => {
+        request(app)
+          .post("/sensor-events/")
+          .set("Authorization", JWT)
+          .send({
+            name: "Test Sensor",
+            location: { latitude: 92.123, longitude: 12.699 },
+            minValue: 90,
+            maxValue: 60,
+          })
+          .expect(200)
+          .end(done);
+      });
+    });
+  });
+
+  it("Should return 400 when trying to save duplicated sensor", function (done) {
+    simpleJWTSign({ payload: "mocking payload" }).then((JWT) => {
+      request(app)
+        .post("/sensors")
+        .set("Authorization", JWT)
+        .send({
+          name: "Test Sensor",
+          location: { latitude: 92.123, longitude: 12.699 },
+          minValue: 90,
+          maxValue: 60,
+        })
+        .expect(400, {
+          message:
+            "There was an error while saving sensor data, probably is because duplicated key.",
+        })
+        .end(done);
+    });
+  });
+});
